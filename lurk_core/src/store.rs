6530f92d5a2ba76df85e3b5874eb943c141c35bc
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-01-10 20:07:50 -0300

    first attempt

--- lurk_core/src/store.rs
+++ lurk_core/src/store.rs
@@ -743,3 +743,51 @@ impl<F: LurkField> Store<F> {
     Ok(())
   }
 }
+
+#[cfg(test)]
+pub mod test {
+
+  use super::*;
+  use blstrs::Scalar as Fr;
+  use ldon::{syntax::Syn, Store as LStore, parser::position::Pos::No};
+
+  #[test]
+  fn equality() {
+    let num_syn = Syn::<Fr>::U64(No, 123);
+    let str_syn = Syn::<Fr>::String(No, "pumpkin".to_string());
+
+    let cache = PoseidonCache::<Fr>::default();
+    let ldon_store = LStore::<Fr>::default();
+
+    let num_cid = ldon_store.insert_syn(&cache, &num_syn);
+    let str_cid = ldon_store.insert_syn(&cache, &str_syn);
+
+    let store = Store::<Fr>::default();
+
+    let num_ptr1 = store.insert_cid(num_cid, &ldon_store).expect("failed to insert num");
+    let str_ptr1 = store.insert_cid(str_cid, &ldon_store).expect("failed to insert str");
+    let cons_ptr1 = store.insert_expr(Expr::Cons(num_ptr1, str_ptr1)).expect("failed to insert cons");
+    let num_expr1 = store.get_expr(&num_ptr1).expect("failed to get num");
+    let str_expr1 = store.get_expr(&str_ptr1).expect("failed to get str");
+    let cons_expr1 = store.get_expr(&cons_ptr1).expect("failed to get cons");
+
+    let num_ptr2 = store.insert_cid(num_cid, &ldon_store).expect("failed to insert num");
+    let str_ptr2 = store.insert_cid(str_cid, &ldon_store).expect("failed to insert str");
+    let cons_ptr2 = store.insert_expr(Expr::Cons(num_ptr2, str_ptr2)).expect("failed to insert cons");
+    let num_expr2 = store.get_expr(&num_ptr2).expect("failed to get num");
+    let str_expr2 = store.get_expr(&str_ptr2).expect("failed to get str");
+    let cons_expr2 = store.get_expr(&cons_ptr2).expect("failed to get cons");
+
+    assert_eq!(num_ptr1, num_ptr2);
+    assert_eq!(str_ptr1, str_ptr2);
+    assert_eq!(cons_ptr1, cons_ptr2);
+
+    assert_eq!(num_expr1, num_expr2);
+    assert_eq!(str_expr1, str_expr2);
+    assert_eq!(cons_expr1, cons_expr2);
+
+    assert!(matches!(cons_expr1, Expr::Cons(num_ptr1, str_ptr1)));
+    assert!(matches!(cons_expr2, Expr::Cons(num_ptr2, str_ptr2)));
+
+  }
+}
