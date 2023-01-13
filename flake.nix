c868c40ac5d9d53e2d9a52e11f27f4a8fe9a9dfa
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2023-01-13 14:14:02 -0500

    Simplify Nix flake (#7)
    Putting the build components in `rust-toolchain.toml` looks cleaner and
    enhances compatibility with non-Nix users. This way, the only
    maintenance going forward should be updating the sha256 hash with the
    error message output if the toolchain is updated.
    
    I also added the `nixpos-unstable` URL to have a common `nixpkgs`
    channel between Nix users, and added the `ocl-icd` dependency for the
    `groth16` feature.
    
    Co-authored-by: John C. Burnham <jcb@johnchandlerburnham.com>

--- flake.nix
+++ flake.nix
@@ -1,50 +1,44 @@
 {
   inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
-  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils, naersk, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
         };
-        target = "aarch64-unknown-linux-gnu";
 
-        toolchain = with fenix.packages.${system}; toolchainOf {
-          channel = "nightly";
-          date = "2023-01-08";
-          sha256 = "sha256-/F36bL5WoJ7opVs7o96dwVHE9SEt3am+6N3jPygJRKY=";
-        };
-          dev-toolchain = toolchain.withComponents [
-            "cargo" "rustc" "rust-src" "rustfmt" "clippy"
-          ];
+        toolchain = with fenix.packages.${system}; fromToolchainFile {
+          file = ./rust-toolchain.toml; # alternatively, dir = ./.;
+          sha256 = "sha256-8t+lcqQLG8xQfN/ygZnrQXON3OI5AmuQ1o8ofIB5RuA=";
+          };
 
       in rec {
-        # For `nix build` & `nix run`:
         defaultPackage = (naersk.lib.${system}.override {
-          cargo = toolchain.rust;
-          rustc = toolchain.rust;
+          # For `nix build` & `nix run`:
+          cargo = toolchain;
+          rustc = toolchain;
         }).buildPackage {
           src = ./.;
-          CARGO_BUILD_TARGET = target;
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
-            "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
         };
 
-        # For `nix develop` (optional, can be skipped):
+        # For `nix develop` or `direnv allow`:
         devShell = pkgs.mkShell {
-          nativeBuildInputs = [
-            dev-toolchain
-            toolchain.rust-analyzer
+          buildInputs = with pkgs; [
+            ocl-icd
+            toolchain
           ];
-          #RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
         };
       }
     );
